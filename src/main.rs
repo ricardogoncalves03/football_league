d361b50dc6985ce7a6f8459f1bdedb11ea50dfe0
Ricardo <ricardogoncalves03@gmail.com>
2023-12-23 15:28:27 +0000

    implement basic match logic for win, draw and lose outcomes

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 mod teams;
+mod matches;
 
 use teams::Team;
+use matches::play_match;
 
 fn main() {
     // Access team details
-    let barcelona = Team::new("Barcelona", "Spain");
-    println!("{:?}", barcelona);
+    let barcelona = &mut Team::new("Barcelona", "Spain");
+    // println!("{:?}", barcelona);
+    let real_madrid = &mut Team::new("Real Madrid", "Spain");
+
+    let result = play_match(barcelona, real_madrid);
+    println!("{:?}", result);
 }
--- src/matches.rs
+++ src/matches.rs
@@ -1,18 +1,32 @@
 // matches.rs could have a Match struct.
 // Employ a Vector in the matches module to track played matches
+use crate::teams::Team;
+
 use rand::random;
 
+#[derive(Debug)]
 pub enum MatchResult {
     Win,
     Draw, 
     Lose,
 }
 
-fn play_match(team1: &Team, team2: &Team) {
+pub fn play_match(team1: &mut Team, team2: &mut Team) -> MatchResult {
     let match_outcome = random::<f32>();
+    println!("{match_outcome}");
 
-    if match_outcome > 0.5 {
+    if match_outcome > 0.66 {
+        // Team 1 wins
         team1.update__score(3);
         MatchResult::Win
+    } else if match_outcome < 0.33 {
+        // Team 2 wins
+        team2.update__score(3);
+        MatchResult::Lose
+    } else {
+        // Draw
+        team1.update__score(1);
+        team2.update__score(1);
+        MatchResult::Draw
     }
 }
